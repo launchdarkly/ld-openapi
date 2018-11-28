bcf6fb9a35f74433a0353ca4cbbb810b232d28ca
Andrew Shannon Brown <ashanbrown@users.noreply.github.com>
2018-11-28 13:55:43 -0800

    Pre release 2.0.10b
    * Add a sample go and node code

--- README-PREFIX.md
+++ README-PREFIX.md
@@ -1,5 +1,5 @@
 This repository contains a client library for LaunchDarkly's REST API. This client was automatically
 generated from our [OpenAPI specification](https://github.com/launchdarkly/ld-openapi).
 
-This REST API is for custom integrations, data export, or automating your feature flag workflows. *DO NOT* use this client library to add feature flags to your web or mobile application. To integrate feature flags with your application, please see the [SDK documentation](https://docs.launchdarkly.com/v2.0/docs)
+This REST API is for custom integrations, data export, or automating your feature flag workflows. *DO NOT* use this client library to include feature flags in your web or mobile application. To integrate feature flags with your application, please see the [SDK documentation](https://docs.launchdarkly.com/v2.0/docs)
 
---
+++ samples/go/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/launchdarkly/api-client-go"
+)
+
+func main() {
+	apiKey := os.Getenv("LD_API_KEY")
+	client := ldapi.NewAPIClient(ldapi.NewConfiguration())
+	ctx := context.WithValue(context.Background(), ldapi.ContextAPIKey, ldapi.APIKey{
+		Key: apiKey,
+	})
+
+	// Create a multi-variate feature flag
+	body := ldapi.FeatureFlagBody{
+		Name: "Test Flag Go",
+		Key:  "test-go",
+		Variations: []ldapi.Variation{
+			{Value: intfPtr([]interface{}{1, 2})},
+			{Value: intfPtr([]interface{}{3, 4})},
+			{Value: intfPtr([]interface{}{5})}}}
+	flag, _, err := client.FeatureFlagsApi.PostFeatureFlag(ctx, "openapi", body, nil)
+	if err != nil {
+		panic(fmt.Errorf("create failed: %s", err))
+	}
+	fmt.Printf("Created flag: %+v\n", flag)
+	// Clean up new flag
+	defer func() {
+		if _, err := client.FeatureFlagsApi.DeleteFeatureFlag(ctx, "openapi", body.Key); err != nil {
+			panic(fmt.Errorf("delete failed: %s", err))
+		}
+	}()
+}
+
+func intfPtr(i interface{}) *interface{} {
+	return &i
+}
