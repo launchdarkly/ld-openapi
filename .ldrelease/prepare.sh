22ad202aa98d943fa7420a72bbcb176d3c605e2e
Ben Woskow <48036130+bwoskow-ld@users.noreply.github.com>
2020-02-07 16:13:21 -0800

    Onboarding releaser (#125)

---
+++ .ldrelease/prepare.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Install Python tools and AWS CLI
+sudo apt update
+sudo apt install awscli python-pip python-setuptools
+sudo pip install wheel twine
+
+# Set up Ruby and RubyGems credentials
+sudo apt-get install ruby
+mkdir -p ~/.gem
+echo -e "---\r\n:rubygems_api_key: $RUBYGEMS_API_KEY" > ~/.gem/credentials
+chmod 0600 /home/circleci/.gem/credentials
+
+# Set up git client
+git config --global user.name $GH_USER
+git config --global user.email $GH_EMAIL
+
+# Fetching credentials
+aws s3 cp s3://launchdarkly-pastebin/ci/openapi/gradle.properties.enc .
+aws s3 cp s3://launchdarkly-pastebin/ci/openapi/secring.gpg.enc .
+openssl enc -d -in gradle.properties.enc -aes-256-cbc -k $ENCRYPTION_SECRET -md md5 > ~/gradle.properties
+openssl enc -d -in secring.gpg.enc -aes-256-cbc -k $ENCRYPTION_SECRET -md md5 > ~/secring.gpg
+
+# Preparing configurations needed for client releases
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
+echo -e "[pypi]\nusername=launchdarkly\npassword=$PYPI_PASSWORD" > ~/.pypirc
