c49e446bcf3559d360a1cee2bcdb648a308bc5ff
LaunchDarklyCI <LaunchDarklyCI@users.noreply.github.com>
2020-12-07 18:59:50 +0000

    prepare 3.10.0 release (#71)

--- scripts/release/java.sh
+++ scripts/release/java.sh
@@ -11,6 +11,5 @@ cp -r ${path} $tmpdir
 cp ${path}/../../scripts/release/java/publish.gradle ${tmpdir}/$(basename ${path})
 
 cd ${tmpdir}/$(basename ${path})
-chmod u+x ./gradlew
-./gradlew --info -b publish.gradle publish closeAndReleaseRepository
-./gradlew --info -b publish.gradle publishGhPages
+gradle --info -b publish.gradle publish closeAndReleaseRepository
+gradle --info -b publish.gradle publishGhPages
