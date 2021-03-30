7dc15ad06f2c25aabd076c6b5b0f24dbbcdaf9cf
Raymond Ho <rho@launchdarkly.com>
2021-03-29 17:02:22 -0700

    fix Python sample

--- samples/python/main.py
+++ samples/python/main.py
@@ -10,30 +10,30 @@ configuration = launchdarkly_api.Configuration()
 configuration.api_key['Authorization'] = os.getenv("LD_API_KEY")
 
 client = launchdarkly_api.ApiClient(configuration)
-api_instance = launchdarkly_api.FeatureFlagsApi(client)
+api_instance = launchdarkly_api.DefaultApi(client)
 
 project_key = "openapi"
 flag_key = "test-python"
 
 # Create a flag with json variations
-feature_flag_body = launchdarkly_api.FeatureFlagBody(
+feature_flag_body = launchdarkly_api.GlobalFlagRep(
     name=flag_key,
     key=flag_key,
     variations=[
-        launchdarkly_api.models.Variation(value=[1, 2]),
-        launchdarkly_api.models.Variation(value=[3, 4]),
-        launchdarkly_api.models.Variation(value=[5])
+        launchdarkly_api.models.VariateRep(value=[1, 2]),
+        launchdarkly_api.models.VariateRep(value=[3, 4]),
+        launchdarkly_api.models.VariateRep(value=[5])
     ])
 
 try:
-    api_response = api_instance.post_feature_flag(project_key, feature_flag_body)
+    api_response = api_instance.api_v2_flags_proj_key_post(feature_flag_body, project_key)
     pprint(api_response)
 except ApiException as e:
     print("Exception creating flag: %s\n" % e)
 
 # Clean up the flag
 try:
-    api_response = api_instance.delete_feature_flag(project_key, flag_key)
+    api_response = api_instance.api_v2_flags_proj_key_key_delete(project_key, flag_key)
     pprint(api_response)
 except ApiException as e:
     print("Exception deleting flag: %s\n" % e)
