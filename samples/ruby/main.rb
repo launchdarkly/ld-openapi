cae275c24c7aef01fdcecb01658e264d6e3e2ef5
Lexi Ross <lross@launchdarkly.com>
2021-03-31 17:13:42 -0700

    update ruby sample code to use new ruby client

--- samples/ruby/main.rb
+++ samples/ruby/main.rb
@@ -6,23 +6,23 @@ LaunchDarklyApi.configure do |config|
   config.api_key['Authorization'] = ENV['LD_API_KEY']
 end
 
-api_instance = LaunchDarklyApi::FeatureFlagsApi.new
+api_instance = LaunchDarklyApi::DefaultApi.new
 
 project_key = "openapi"
 flag_key = "test-ruby"
 
 # Create a flag with a json variations
-body = LaunchDarklyApi::FeatureFlagBody.new(
+body = LaunchDarklyApi::GlobalFlagRep.new(
   name: "test-ruby",
   key: flag_key,
   variations: [
-    LaunchDarklyApi::Variation.new(value=[1,2]),
-    LaunchDarklyApi::Variation.new(value=[3,4]),
-    LaunchDarklyApi::Variation.new(value=[5]),
+    LaunchDarklyApi::VariateRep.new(value=[1,2]),
+    LaunchDarklyApi::VariateRep.new(value=[3,4]),
+    LaunchDarklyApi::VariateRep.new(value=[5]),
   ])
 
 begin
-  result = api_instance.post_feature_flag(project_key, body)
+  result = api_instance.api_v2_flags_proj_key_post(project_key, body)
   p result
 rescue LaunchDarklyApi::ApiError => e
   puts "Exception creating feature flag: #{e}"
@@ -30,7 +30,7 @@ end
 
 # Clean up new flag
 begin
-  result = api_instance.delete_feature_flag(project_key, flag_key)
+  result = api_instance.api_v2_flags_proj_key_key_delete(project_key, flag_key)
   p result
 rescue LaunchDarklyApi::ApiError => e
   puts "Exception deleting feature flag: #{e}"
